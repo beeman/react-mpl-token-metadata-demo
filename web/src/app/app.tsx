70cc7a6e3fe947b6362f704d545483258619c034
Bram Borggreve <beeman@beeman.nl>
2024-01-16 00:38:12 +0000

    feat: working example of importing createMetadataAccountV3

--- web/src/app/app.tsx
+++ web/src/app/app.tsx
@@ -1,12 +1,14 @@
-import { UiLayout } from './ui/ui-layout';
+import { createMetadataAccountV3 } from '@metaplex-foundation/mpl-token-metadata';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRoutes } from './app-routes';
 import { ClusterProvider } from './cluster/cluster-data-access';
 import { SolanaProvider } from './solana/solana-provider';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UiLayout } from './ui/ui-layout';
 
 const client = new QueryClient();
 
 export function App() {
+  console.log('createMetadataAccountV3', createMetadataAccountV3);
   return (
     <QueryClientProvider client={client}>
       <ClusterProvider>
